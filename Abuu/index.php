4f9a8cd4bed65fc7e7711088f2aa69cd021310df
Freemano <freemano@MACs-MBP.mshome.net>
2024-02-02 16:48:16 +0300

    adde one folder

---
+++ Abuu/index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi consequuntur, cum delectus incidunt iste laborum non officiis optio quaerat quam quos rem vitae, voluptate. Aliquid distinctio dolorum, enim incidunt nesciunt optio quibusdam totam! Cumque delectus dolore iure maxime molestias necessitatibus omnis placeat tempore? Ab odio quaerat quam quidem ratione tenetur. Aliquam inventore quis quisquam rem repellat. Accusantium autem debitis dignissimos distinctio dolorem, doloremque dolores ea eos id inventore ipsum mollitia nam officiis possimus quasi quod reiciendis reprehenderit similique sit sunt suscipit tempora ut vero vitae voluptatibus! Aperiam assumenda at blanditiis, commodi, cupiditate dolor id illo in molestias nam optio rem!</p>
+</body>
+</html>
